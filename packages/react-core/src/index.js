96e5f1b1ed86c64a947e01c58d63157004857ac7
Klevis Cipi <cipiklevis@gmail.com>
2018-08-20 18:05:30 +0200

    merge

--- packages/react-core/src/index.js
+++ packages/react-core/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Root from './core/route/index.js';
-import createStore from './core/store/configureStore.js'
+import Root             from './app/core/route/index.js';
+import createStore      from './app/core/store/configureStore.js'
+import environment      from './environment/environment.js'
+
+/***INCLUDE BOOTSTRAP***/
+
+import 'bootstrap';
+import './assets/css/style.css'
+
+window.environment = environment
 
 const initialState = {}
 const store = createStore(initialState)
 
-let render = () => {
+let init = () => {
 	ReactDOM.render(<Root store={store} />,
 		document.getElementById('root')
 	);
 }
 
-render();
+init();
